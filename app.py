9c6438aa74f7a6d6b07cccd68b6d018b8238e256
Tkai87 <tkai@usc.edu>
2022-01-31 04:07:55 -0800

    heroku psql

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ app = Flask(__name__)
 # URI = 'postgresql://' + USERNAME + ':' + PASSWORD + '@localhost/todo'
 # URI = 'postgresql://kaitetsuro:majestyc@localhost/todo'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kaitetsuro:majestyc@localhost/todo'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://kaitetsuro:majestyc@localhost/todo'
 # heroku = Heroku(app)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://uguzpaowiqtzvi:e9252b774c264bc70a242ab4bc9f081d14bc134145144f0eebab6ae1263c878a@ec2-3-212-143-188.compute-1.amazonaws.com:5432/d8i7dlato8vnhn'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://uguzpaowiqtzvi:e9252b774c264bc70a242ab4bc9f081d14bc134145144f0eebab6ae1263c878a@ec2-3-212-143-188.compute-1.amazonaws.com:5432/d8i7dlato8vnhn'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 db = SQLAlchemy(app)
 
