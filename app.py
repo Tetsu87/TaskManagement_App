a1bc250088e1949c32dc589f6858b7eeb0c8a73a
Tkai87 <tkai@usc.edu>
2022-01-31 15:53:23 -0800

    back to previous ver

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ app = Flask(__name__)
 # URI = 'postgresql://' + USERNAME + ':' + PASSWORD + '@ec2-3-212-143-188.compute-1.amazonaws.com:5432/d8i7dlato8vnhn'
 
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://uguzpaowiqtzvi:e9252b774c264bc70a242ab4bc9f081d14bc134145144f0eebab6ae1263c878a@ec2-3-212-143-188.compute-1.amazonaws.com:5432/d8i7dlato8vnhn'
-app.config['SQLALCHEMY_DATABASE_URI'] = URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://uguzpaowiqtzvi:e9252b774c264bc70a242ab4bc9f081d14bc134145144f0eebab6ae1263c878a@ec2-3-212-143-188.compute-1.amazonaws.com:5432/d8i7dlato8vnhn'
+# app.config['SQLALCHEMY_DATABASE_URI'] = str(URI)
 
 db = SQLAlchemy(app)
 
